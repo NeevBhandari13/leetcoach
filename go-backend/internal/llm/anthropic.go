716b2411b2c651bbcab581713edc2d75b07cfaa4
NeevBhandari13 <86910225+NeevBhandari13@users.noreply.github.com>
2026-05-01 18:20:34 +1000

    Added sending for anthropic messages

--- go-backend/internal/llm/anthropic.go
+++ go-backend/internal/llm/anthropic.go
@@ -1,1 +1,66 @@
 package llm
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/anthropics/anthropic-sdk-go"
+	"github.com/anthropics/anthropic-sdk-go/option"
+)
+
+type AnthropicClient struct {
+	client anthropic.Client
+	model  anthropic.Model
+}
+
+func NewAnthropicClient(apiKey string, anthropicModel anthropic.Model) *AnthropicClient {
+	return &AnthropicClient{
+		client: anthropic.NewClient(
+			option.WithAPIKey(apiKey),
+		),
+		model: anthropicModel,
+	}
+}
+
+func toAnthropicMessages(messages []Message) []anthropic.MessageParam {
+	anthropicMessages := make([]anthropic.MessageParam, len(messages))
+	for idx, message := range messages {
+		anthropicMessages[idx] = toAnthropicMessage(message)
+	}
+
+	return anthropicMessages
+}
+
+func toAnthropicMessage(message Message) anthropic.MessageParam {
+	block := anthropic.NewTextBlock(message.Content)
+	if message.Role == "assistant" {
+		return anthropic.NewAssistantMessage(block)
+	} else {
+		return anthropic.NewUserMessage(block)
+	}
+}
+
+func (c *AnthropicClient) Send(ctx context.Context, system string, messages []Message) (string, error) {
+	response, err := c.client.Messages.New(ctx, anthropic.MessageNewParams{
+		MaxTokens: 1024,
+		Messages:  toAnthropicMessages(messages),
+		System: []anthropic.TextBlockParam{{
+			Text: system,
+		}},
+		Model: c.model,
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	for _, block := range response.Content {
+		switch v := block.AsAny().(type) {
+		case anthropic.TextBlock:
+			return v.Text, nil
+		}
+	}
+
+	return "", fmt.Errorf("no text in response")
+
+}
