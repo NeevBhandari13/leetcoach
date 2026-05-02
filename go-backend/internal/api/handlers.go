4242d6140ebd1a1abfcc91dcc3b024d29977e167
NeevBhandari13 <86910225+NeevBhandari13@users.noreply.github.com>
2026-05-02 21:14:06 +1000

    Added ChatHandler

--- go-backend/internal/api/handlers.go
+++ go-backend/internal/api/handlers.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/NeevBhandari13/leetcoach/internal/chat"
 	"github.com/NeevBhandari13/leetcoach/internal/llm"
 	"github.com/gin-gonic/gin"
@@ -17,6 +19,33 @@ type ChatResponse struct {
 
 func ChatHandler(chatService *chat.ChatService) gin.HandlerFunc {
 	return func(c *gin.Context) {
+		// variable to hold the request body
+		var req ChatRequest
+
+		// tries to map request into ChatRequest
+		// if the request body doesnt match the definition, we
+		// throw an error
+		err := c.ShouldBindJSON(&req)
+		if err != nil {
+			// gin.H creates a simple json
+			c.JSON(http.StatusBadRequest, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		resp, err := chatService.Reply(c, req.System, req.Messages)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+			return
+		}
+
+		c.JSON(http.StatusOK, ChatResponse{
+			Message: resp,
+		})
+		return
 
 	}
 }
