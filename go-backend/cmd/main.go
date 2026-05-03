aaab6d06bc8f80b199cdea1db7c0656d75b76b7e
NeevBhandari13 <86910225+NeevBhandari13@users.noreply.github.com>
2026-05-03 10:02:34 +1000

    added main.go to wire everything up

--- go-backend/cmd/main.go
+++ go-backend/cmd/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"os"
+
+	"github.com/NeevBhandari13/leetcoach/internal/api"
+	"github.com/NeevBhandari13/leetcoach/internal/chat"
+	"github.com/NeevBhandari13/leetcoach/internal/llm"
+	"github.com/anthropics/anthropic-sdk-go"
+)
+
+func main() {
+	client := NewLLMClient()                   // llm client interface
+	chatService := chat.NewChatService(client) // chat service with the reply function
+	router := api.NewRouter(chatService)       // creates new router
+	router.Run(":8080")                        // router on port 8080
+
+}
+
+func NewLLMClient() llm.Client {
+	provider := os.Getenv("LLM_PROVIDER")
+
+	switch provider {
+	case "anthropic":
+		return llm.NewAnthropicClient(os.Getenv("ANTHROPIC_API_KEY"), anthropic.Model(os.Getenv("LLM_MODEL")))
+	default:
+		panic("Invalid LLM Provider")
+
+	}
+}
